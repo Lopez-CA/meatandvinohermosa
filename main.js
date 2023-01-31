09d928e0ef86eae9b2a55dbf72ccbd4d3612a8ba
Lopez-CA <56131630+Lopez-CA@users.noreply.github.com>
2023-01-31 04:10:44 -0800

    added category menu images

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ hamburger.addEventListener('click', function () {
   this.classList.toggle ('is-active')
 
 });
+
+// Add click event listeners to image blocks
+const categories = document.querySelectorAll('.category a');
+categories.forEach(category => {
+  category.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Add your custom logic here
+  });
+});
