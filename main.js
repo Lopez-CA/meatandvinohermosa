bed39e1c3f0efa108dc1850f00b6c95424c3b5af
Lopez-CA <56131630+Lopez-CA@users.noreply.github.com>
2023-02-07 09:43:40 -0800

    pages linked

--- main.js
+++ main.js
@@ -19,37 +19,5 @@ document.querySelectorAll(".nav-link").forEach(n => n.
 
 
 
-
-
-
-
 // Add click event listeners to image blocks
-const categories = document.querySelectorAll('.category a');
-categories.forEach(category => {
-  category.addEventListener('click', function(e) {
-    e.preventDefault();
-    // Add your custom logic here
-  });
-});
 
-const icons = document.querySelectorAll(".icon");
-
-icons.forEach(icon => {
-  icon.addEventListener("mouseenter", function() {
-    this.style.transform = "scale(1.2)";
-  });
-  
-  icon.addEventListener("mouseleave", function() {
-    this.style.transform = "scale(1)";
-  });
-});
-
-
-$(document).ready(function() {
-  $("#newsletter-form").submit(function(event) {
-    event.preventDefault();
-    var email = $("#email-input").val();
-    // Send a request to your server to store the email address
-    // ...
-  });
-});
